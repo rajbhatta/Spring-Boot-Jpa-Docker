2dd6c3f0af9c85d94c0a8fefb1fa4dddf9988d81
Raj Bhatta <raj@sstwireless.com>
2022-03-11 11:08:16 -0800

    Task 20: CORS communication between client and server is rechecked

--- src/main/java/com/guusto/restapi/controller/ClientController.java
+++ src/main/java/com/guusto/restapi/controller/ClientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8085")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1/guusto-service")
 public class ClientController {
--- src/main/java/com/guusto/restapi/controller/GiftcardController.java
+++ src/main/java/com/guusto/restapi/controller/GiftcardController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:8085")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1/guusto-service")
 public class GiftcardController {
